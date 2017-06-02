38712b675c8ae30d3e0bd56294b1d2594f86af47
clarkscrandis <clark_scrandis@hotmail.com>
2017-06-02 22:16:28 +0100

    Added ability to update users, and forced refresh on writes to ES.

--- users.py
+++ users.py
@@ -1,17 +1,12 @@
 import sys
 import json
-from collections import namedtuple
+#from collections import namedtuple
 from elasticsearch import Elasticsearch
 import pprint #PrettyPrinter
 
 REMOTE_DBG = False
 ES_HOST = 'localhost'
 ES_PORT = 9200
-
-# Call this from the commandline using the following syntax: python users.py add "[{\"key\":\"value1\"}, {\"key\":\"value2\"}]"
-# python users.py add "[{\"firstName\":\"value1\"}]"
-
-findAllUsersQuery = { 'query': { 'match_all': {} } }
 
 def findUserQuery(emailValue):
     return { 'query': {
@@ -28,44 +23,8 @@ def findUserQuery(emailValue):
 m_pp = pprint.PrettyPrinter(indent=4)
 m_es = Elasticsearch([{'host':ES_HOST, 'port':ES_PORT}])
 
-# Confirm ES is running and if the user index not yet created, set up the appropriate mapping
-#python users.py init "{}"
-def initUserStore():
-    responseTxt = 'NOT OK'
-    userTypeMapping = {
-      "mappings": {
-        "userType" : {
-          "properties" : {
-            "firstName" : {
-              "type" :    "string"
-            },
-            "lastName" : {
-              "type" :   "string"
-            },
-            "email" : {
-              "type" :   "string",
-              "index" : "not_analyzed"
-            }
-          }
-        }
-      }
-                        }
-    try:
-        userStoreExists = m_es.indices.exists(index='user')
-        responseTxt = 'OK'
-    except:
-        sys.stderr.write("Please fix connection to Elasticsearch: " + ES_HOST + ":" + str(ES_PORT) + "\n")
-        sys.exit(1)
-    if not(userStoreExists):
-        res = m_es.indices.create(index='user', body=userTypeMapping)
-        if res['acknowledged'] == True:
-            responseTxt = 'OK'
-        else:
-            sys.stderr.write("Unable to define the mapping needed by the user store")
-            sys.exit(1)
-    return responseTxt
-
 # python users.py add "[{\"firstName\":\"bogusFirstName\",\"lastName\":\"bogusLastName\",\"email\":\"name@company.com\"}]"
+# NOTE: email needs to be unique among users. If a user already has the provided email address, then a new user will NOT be created. 
 def addUsers(jsonTxt):
     userList = json.loads(jsonTxt)
     #x = json.loads(jsonTxt, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
@@ -108,7 +67,7 @@ def deleteUsers(jsonTxt):
     for user in userList:
         #Might not need this try/except logic if we upgrade to ES 5.x
         try:
-            res = m_es.delete(index='user', doc_type='userType', id=user['userId'])
+            res = m_es.delete(index='user', doc_type='userType', id=user['userId'], refresh=True)
             #m_pp.pprint(res)
             if res['found'] == True:
                 response['deletedUserIdList'].append(user)
@@ -119,6 +78,24 @@ def deleteUsers(jsonTxt):
     responseTxt = json.dumps(response)
     return responseTxt
     
+#python users.py getAll "{}"
+def getAllUsers():
+    findAllUsersQuery = { 'query': { 'match_all': {} } }
+
+    response = { 'foundUserList' : [],
+                 'missingUserIdList' : [],
+                 'errorUserIdList' : []
+        }
+    
+    searchRes = m_es.search(index='user', doc_type='userType', body=findAllUsersQuery)
+    if searchRes['hits']['total'] > 0:
+        userList = searchRes['hits']['hits']
+        for user in userList:
+            user['_source']['userId'] = user['_id']
+            response['foundUserList'].append(user['_source'])   
+    jsonTxt2 = json.dumps(response)
+    return jsonTxt2
+
 # python users.py get "[{\"userId\":\"123\"}]"
 def getUsers(jsonTxt):
     response = { 'foundUserList' : [],
@@ -140,21 +117,87 @@ def getUsers(jsonTxt):
     jsonTxt2 = json.dumps(response)
     return jsonTxt2
     
-#python users.py getAll "{}"
-def getAllUsers():
-    response = { 'foundUserList' : [],
-                 'missingUserIdList' : [],
-                 'errorUserIdList' : []
+# Confirm ES is running and if the user index not yet created, set up the appropriate mapping
+#python users.py init "{}"
+def initUserStore():
+    responseTxt = 'NOT OK'
+    userTypeMapping = {
+      "mappings": {
+        "userType" : {
+          "properties" : {
+            "companyId" : {
+              "type" :    "string"
+            },
+            "contractIdSet" : {
+              "type" :    "string"
+            },
+            "email" : {
+              "type" :   "string",
+              "index" : "not_analyzed"
+            },
+            "firstName" : {
+              "type" :    "string"
+            },
+            "lastName" : {
+              "type" :   "string"
+            },
+            "permissionSet" : {
+              "type" :   "string"
+            },
+            "searchIdSet" : {
+              "type" :    "string"
+            },
+            "resumeSet" : {
+              "type" :    "string"
+            },
+            "roleSet" : {
+              "type" :    "string"
+            }
+          }
         }
+      }
+                        }
+    try:
+        userStoreExists = m_es.indices.exists(index='user')
+        responseTxt = 'OK'
+    except Exception, e:
+        sys.stderr.write("Please fix connection to Elasticsearch: " + ES_HOST + ":" + str(ES_PORT) + "\n")
+        sys.stderr.write(str(e))
+        sys.exit(1)
+    if not(userStoreExists):
+        res = m_es.indices.create(index='user', body=userTypeMapping, refresh=True)
+        if res['acknowledged'] == True:
+            responseTxt = 'OK'
+        else:
+            sys.stderr.write("Unable to define the mapping needed by the user store\n")
+            sys.exit(1)
+    return responseTxt
+
+# python users.py update "{\"userId\":\"123\", \"firstName\":\"bogusFirstName\",\"lastName\":\"bogusLastName\",\"email\":\"name@company.com\", \"permissionSet\":[\"str3\", \"str2\"]}"
+# Update the list of fields that are provided. If you want to remove fields, then a different function will be necessary.
+# NOTE: I expect the webserver/user will correctly format and structure the json that is provided.
+def updateUser(jsonTxt):
+    #TODO: Perform error handling if there is a problem with adding the data back in
+    updatedUser = json.loads(jsonTxt)
     
-    searchRes = m_es.search(index='user', doc_type='userType', body=findAllUsersQuery)
-    if searchRes['hits']['total'] > 0:
-        userList = searchRes['hits']['hits']
-        for user in userList:
-            user['_source']['userId'] = user['_id']
-            response['foundUserList'].append(user['_source'])   
-    jsonTxt2 = json.dumps(response)
-    return jsonTxt2
+    # Extract and remove userId out of the user record.
+    userId = updatedUser['userId']
+    del updatedUser['userId']
+
+    try:
+        # Get the current user record and update it with the values that were provided.
+        user = m_es.get_source(index='user', doc_type='userType', id=userId)
+        user.update(updatedUser)
+     
+        # Write the user record back out.
+        m_es.index(index='user', doc_type='userType', body=user, id=userId, refresh=True) 
+    except Exception, e:
+        sys.stderr.write("Unable to find or update the specified user: " + userId + "\n")
+        sys.stderr.write(str(e))
+        sys.exit(1)
+
+    return (jsonTxt)
+    
     
 if __name__ == '__main__':
     if REMOTE_DBG:
@@ -174,26 +217,22 @@ if __name__ == '__main__':
         #print ('python says: '+ sys.argv[1])
         func = sys.argv[1]
         inputData = sys.argv[2]
-    elif len(sys.argv) == 2:
-        print ('in python')
-        #inputData = sys.argv[1]['args'][0]
-        #print func
-        #print inputData
-        sys.exit()
     else:
-        sys.stderr.write('You didn\'t provide enough values')
+        sys.stderr.write('You need to pass at least three values to python: [module] [command] [json data]\n')
         sys.exit(1)
     
     if func == 'add':
         print(addUsers(inputData))
     elif func == 'delete':
         print(deleteUsers(inputData))
-    elif func == 'get':
-        print(getUsers(inputData))
     elif func == 'getAll':
         print(getAllUsers())
+    elif func == 'get':
+        print(getUsers(inputData))
     elif func == 'init':
         print(initUserStore())
+    elif func == 'update':
+        print(updateUser(inputData))
     else:
-        sys.stderr.write('You asked an unsupported function: ' + func)
+        sys.stderr.write('You specified an unsupported command: ' + func)
         sys.exit(1)
